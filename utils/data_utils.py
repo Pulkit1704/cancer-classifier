32b54b7df68e52930a52919a473c366fe3731141
Pulkit <pgoel3379@gmail.com>
2025-07-25 13:27:52 +0530

    move metadataprocess to utils

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,10 +1,3 @@
-import pandas as pd 
-import sklearn 
-import imblearn
-import numpy as np 
-import re 
-
-
 def get_metadata(filepath: str) -> list: 
 
     try: 
@@ -20,3 +13,11 @@ def filter_characteristic(metadata: list, keyword: str)-> list:
 
     return list(filter(lambda x: keyword in x, metadata))
 
+
+def filter_diagnosis_string(diagnosis_str: str,  keyword: str = "diagnosis")-> list: 
+
+    diagnosis_list  = list(filter(lambda x: keyword in x, diagnosis_str.split("\t")[1:]))
+
+    diagnosis_list = list(map(lambda x: x.strip("\""), diagnosis_list))
+    
+    return diagnosis_list
